723882d788153ef34eb1165a773d9f79daf37d53
Rodrigo Vilar <67280390+rodrigodcx@users.noreply.github.com>
2020-09-20 01:12:33 -0300

    Initial commit

---
+++ cupom.test.ts
@@ -0,0 +1,60 @@
+const cupom = require('./cupom');
+
+test('Exercício 1', () => {
+  expect(cupom.imprime_dados_loja()).toBe(
+    `Arcos Dourados Com. de Alimentos LTDA
+Av. Projetada Leste, 500 EUC F32/33/34
+Br. Sta Genebra - Campinas - SP
+CEP:13080-395 Tel (19) 3756-7408
+Loja 1317 (PDP)
+CNPJ: 42.591.651/0797-34
+IE: 244.898.500.113
+`);
+});
+
+test('Exercício 2 - tudo vazio', () => {
+
+  cupom.dados.nome_loja = "";
+  cupom.dados.logradouro = "";
+  cupom.dados.numero = 0;
+  cupom.dados.complemento = "";
+  cupom.dados.bairro = "";
+  cupom.dados.municipio = "";
+  cupom.dados.estado = "";
+  cupom.dados.cep = "";
+  cupom.dados.telefone = "";
+  cupom.dados.observacao = "";
+  cupom.dados.cnpj = "";
+  cupom.dados.inscricao_estadual = "";
+
+  expect(cupom.imprime_dados_loja()).toBe(
+    `
+    , 0 
+     -  - 
+    CEP: Tel 
+    CNPJ: 
+    IE: 
+`);
+});
+
+test('Exercício 2 - customizado', () => {
+
+  // Defina seus próprios valores para as variáveis a seguir
+  cupom.dados.nome_loja = "";
+  cupom.dados.logradouro = "";
+  cupom.dados.numero = 0;
+  cupom.dados.complemento = "";
+  cupom.dados.bairro = "";
+  cupom.dados.municipio = "";
+  cupom.dados.estado = "";
+  cupom.dados.cep = "";
+  cupom.dados.telefone = "";
+  cupom.dados.observacao = "";
+  cupom.dados.cnpj = "";
+  cupom.dados.inscricao_estadual = "";
+
+  //E atualize o texto esperado abaixo
+  expect(cupom.imprime_dados_loja()).toBe(
+    `
+`);
+});
